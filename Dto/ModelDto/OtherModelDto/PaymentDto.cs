cf358c5d8e095785f3eb32a88e3b1b1f5ef96c52
Bui Duc Khanh <khanhbuiduc44@gmail.com>
2024-07-02 00:01:48 +0700

    add Paid controller by parent

---
+++ Dto/ModelDto/OtherModelDto/PaymentDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CenterEnglishManagement.Dto.ModelDto.OtherModelDto
+{
+    public class PaymentDto { 
+    
+        public int StudentId { get; set; }
+        public int Amount { get; set; }
+        public string Date { get; set; }
+    }
+}
